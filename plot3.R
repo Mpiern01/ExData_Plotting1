b5b5a62732d32f19a57c848076c12acfda8e8e24
Mpiern01 <Marcpierne@gmail.com>
2015-07-12 13:02:00 -0700

    plot_3

---
+++ plot3.R
@@ -0,0 +1,16 @@
+## Download the main dataset from <b>Dataset</b>: <a href="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip">Electric power consumption</a> [20Mb]
+## Unzip, save it, then run the script. It was too large to commit to Github. Sorry ;(epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
+
+epc$DateTime = paste(epc$Date, epc$Time)
+epc$DateTime = as.POSIXlt(epc$DateTime,format="%d/%m/%Y %H:%M:%S")
+epc$Date = NULL
+epc$Time = NULL
+
+subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+
+png("PNG/plot3.png")
+plot(x=(subepc$DateTime),y=subepc$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
+lines(x=(subepc$DateTime),y=subepc$Sub_metering_2,col="red")
+lines(x=(subepc$DateTime),y=subepc$Sub_metering_3,col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",col=c("black","red","blue"),lwd=2,cex=0.7)
+dev.off()
